24b4ef68fbd38def0f56a73ba60f37973952821a
Andrey A <1993877+krishnathon2006@users.noreply.github.com>
2026-07-12 09:54:24 -0700

    Add Week 2 homework template

---
+++ weeks/week_02/app/config.py
@@ -0,0 +1,4 @@
+DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost:7432/postgres"
+PAYMENT_API_URL = "http://localhost:9001"
+PROTECTION_API_URL = "http://localhost:9002"
+BOOKING_TTL_MINUTES = 15
